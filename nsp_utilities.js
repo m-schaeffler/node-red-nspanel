b156c2106fd30b5c797e6af41ea2f1d11b4dd6eb
Mathias Schäffler <mathias@fam-schaeffler.de>
2023-01-09 09:09:18 +0100

    Farb-Konstanten

---
+++ nsp_utilities.js
@@ -0,0 +1,15 @@
+// NSP-Utilities for use in NodeRed function nodes
+
+// Farben
+let constants = {
+    colBlack:  0x0000,
+    colGray:   0x8C51,
+    colWhite:  0xFFFF,
+    colRed:    0xF800,
+    colYellow: 0xFFC0,
+    colGreen:  0x07C0,
+    colBlue:   0x422F,
+    colAqua:   0x07FF
+};
+
+module.exports = Object.freeze(constants);
