96e202b9f9db078fc7c46f386211dbb43346c0ed
Mathias Schäffler <mathias@fam-schaeffler.de>
2023-01-09 11:31:29 +0100

    icons

--- nsp_utilities.js
+++ nsp_utilities.js
@@ -10,6 +10,30 @@ exports.colGreen  = 0x07C0;
 exports.colBlue   = 0x422F;
 exports.colAqua   = 0x07FF;
 
+// Icons
+exports.iconWeatherSunny = "";
+exports.iconWeatherNight = "";
+exports.iconWeatherCloudy = "";
+exports.iconWeatherPartlyCloudy = "";
+exports.iconWeatherNightPartlyCloudy = "";
+exports.iconWeatherFog = "";
+exports.iconWeatherHail = "";
+exports.iconWeatherLightning = "";
+exports.iconWeatherRainy = "";
+exports.iconWeatherPartlyRainy = "";
+exports.iconWeatherSnowy = "";
+exports.iconWifi = "";
+exports.iconWifiLock = "";
+exports.iconWifiLockOpen = "";
+exports.iconWifiOff = "";
+exports.iconWifiStrength1 = "";
+exports.iconWifiStrength2 = "";
+exports.iconWifiStrength3 = "";
+exports.iconWifiStrength4 = "";
+exports.iconWifiStrengthOff = "";
+exports.iconWifiStrengthOffOutline = "";
+exports.iconWifiStrengthOutline = "";
+
 // Functions for message construction
 
 exports.addEntity = function(type,entity,icon,color,label="",value="")
